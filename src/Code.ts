b2bcab242cccc784221a5ad43ec919b255e153f6
tjifu <yzktjf04@gmail.com>
2020-06-13 21:16:18 +0900

    lint

--- src/Code.ts
+++ src/Code.ts
@@ -1,42 +1,42 @@
 const sendLine = (message: string): void => {
-  const token = 'xxxxxxx';
-  const payload = { message };
+  const token = 'xxxxxxx'
+  const payload = { message }
   const options = {
     method: 'post',
     payload,
-    headers: { Authorization: `Bearer ${token}` }
-  };
-  UrlFetchApp.fetch('https://notify-api.line.me/api/notify', options);
-};
+    headers: { Authorization: `Bearer ${token}` },
+  }
+  UrlFetchApp.fetch('https://notify-api.line.me/api/notify', options)
+}
 
 const fetchContactMail = (): string[] => {
   // 取得間隔
-  const now = Math.floor(new Date().getTime() / 1000);
-  const interval = 30; // 30分前〜現在の新着メールを取得
-  const term = now - 60 * interval;
+  const now = Math.floor(new Date().getTime() / 1000)
+  const interval = 30 // 30分前〜現在の新着メールを取得
+  const term = now - 60 * interval
 
-  const searchCondition = `(is:unread after: ${term})`;
+  const searchCondition = `(is:unread after: ${term})`
 
-  const threads = GmailApp.search(searchCondition);
-  const messages = GmailApp.getMessagesForThreads(threads);
-  const displayMessages = messages.map(message => {
-    const msg = message.slice(-1)[0];
-    msg.markRead();
+  const threads = GmailApp.search(searchCondition)
+  const messages = GmailApp.getMessagesForThreads(threads)
+  const displayMessages = messages.map((message) => {
+    const msg = message.slice(-1)[0]
+    msg.markRead()
 
     return `\n【date】${msg.getDate()}
             \n【From】${msg.getFrom()}
             \n【Subject】${msg.getSubject()}
-            \n【Body】${msg.getPlainBody().slice(0, 200)}`;
-  });
+            \n【Body】${msg.getPlainBody().slice(0, 200)}`
+  })
 
-  return displayMessages;
-};
+  return displayMessages
+}
 
 function main(): void {
-  const newMessages = fetchContactMail();
+  const newMessages = fetchContactMail()
   if (newMessages.length > 0) {
-    newMessages.forEach(message => {
-      sendLine(message);
-    });
+    newMessages.forEach((message) => {
+      sendLine(message)
+    })
   }
 }
